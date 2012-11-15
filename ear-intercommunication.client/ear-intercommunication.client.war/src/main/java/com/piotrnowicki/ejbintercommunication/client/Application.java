db0d0819609f5fb96f1970bee93a9d3de4f79a0d
PiotrNowicki <piotr@piotrnowicki.com>
2012-11-15 18:00:24 +0100

    added WAR

---
+++ ear-intercommunication.client/ear-intercommunication.client.war/src/main/java/com/piotrnowicki/ejbintercommunication/client/Application.java
@@ -0,0 +1,9 @@
+package com.piotrnowicki.ejbintercommunication.client;
+
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class Application extends javax.ws.rs.core.Application {
+
+}
---
+++ ear-intercommunication.client/ear-intercommunication.client.war/src/main/java/com/piotrnowicki/ejbintercommunication/client/Entrypoint.java
@@ -0,0 +1,21 @@
+package com.piotrnowicki.ejbintercommunication.client;
+
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/")
+public class Entrypoint {
+
+    @EJB
+    Client client;
+    
+	@Path("invoke")
+	@GET
+	public String invoke() throws NamingException {
+	    return client.invoke();
+	}
+}
